a75c8ac3bfa3d6c8c391f7ddbe298d64c0e56f52
Omar Karake <omar.karake@amalitechtraining.org>
2025-02-04 20:03:22 +0200

    two different way of fetching data: observables to promise and promise, converting observable to promise

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -1,14 +1,16 @@
-import {Injectable} from "@angular/core";
-import {environment} from "../../environments/environment";
-import {Course} from "../models/course.model";
-
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Course } from '../models/course.model';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root',
 })
 export class CoursesServiceWithFetch {
-
   env = environment;
-
-
+  loadAllCourses = async (): Promise<Course[] | undefined> => {
+      const response = await fetch(`${this.env.apiRoot}/courses`);
+      console.log("response: ", response)
+      const payload = await response.json();
+      return payload.courses;
+  };
 }
