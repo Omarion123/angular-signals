9425b8c0e6288688cd9c3a754e20d0fa92b02827
Omar Karake <omar.karake@amalitechtraining.org>
2025-02-04 17:08:22 +0200

    feat(signalvalue) angular effect and signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,5 @@
-import {
-  Component,
-  computed,
-  effect,
-  inject,
-  Injector,
-  signal,
-} from '@angular/core';
+import { afterNextRender, computed } from '@angular/core';
+import { Component, effect, inject, Injector, signal } from '@angular/core';
 import { CoursesService } from '../services/courses.service';
 import { Course, sortCoursesBySeqNo } from '../models/course.model';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
@@ -30,8 +24,36 @@ type Counter = {
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  counter = signal<number[]>([0]);
-  append() {
-    this.counter.update((values) => [...values, values[values.length - 1] + 1]);
+  counter = signal(0);
+
+  constructor(private readonly injector: Injector) {
+    afterNextRender(() => {
+      // effect inside a function or any life-cycle hook will cause an error
+      effect(
+        () => {
+          // effect in constructor directly, without any method or life-cycle hook will not cause an issue
+          //   effect are used very rarely
+          console.log(`couner value: ${this.counter()}`);
+        },
+        {
+          // because we using effect inside afterNextRender, we will tell angular about this effect
+          injector: injector,
+        }
+      );
+    });
+  }
+
+  tenXCounter = computed(() => {
+    const val = this.counter();
+    return val * 10;
+  });
+
+  hundredXCounter = computed(() => {
+    const val = this.tenXCounter();
+    return val * 10;
+  });
+
+  increment() {
+    this.counter.set(this.counter() + 1);
   }
 }
