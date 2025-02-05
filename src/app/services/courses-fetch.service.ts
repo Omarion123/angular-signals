81bb2cae373126a2dc5977319efe6d74a76df22a
Omar Karake <omar.karake@amalitechtraining.org>
2025-02-05 10:48:30 +0200

    full crud using fetch as old js syntax

--- src/app/services/courses-fetch.service.ts
+++ src/app/services/courses-fetch.service.ts
@@ -8,9 +8,41 @@ import { Course } from '../models/course.model';
 export class CoursesServiceWithFetch {
   env = environment;
   loadAllCourses = async (): Promise<Course[] | undefined> => {
-      const response = await fetch(`${this.env.apiRoot}/courses`);
-      console.log("response: ", response)
-      const payload = await response.json();
-      return payload.courses;
+    const response = await fetch(`${this.env.apiRoot}/courses`);
+    console.log('response: ', response);
+    const payload = await response.json();
+    return payload.courses;
+  };
+
+  createCourse = async (course: Partial<Course>): Promise<Course> => {
+    const reponse = await fetch(`${this.env.apiRoot}/courses`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify(course),
+    });
+
+    return await reponse.json();
+  };
+
+  saveCourse = async (
+    courseId: string,
+    changes: Partial<Course>
+  ): Promise<Course> => {
+    const reponse = await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify(changes),
+    });
+    return await reponse.json();
+  };
+
+  deleteCourse = async (courseId: string): Promise<void> => {
+    await fetch(`${this.env.apiRoot}/courses/${courseId}`, {
+      method: 'DELETE',
+    });
   };
 }
