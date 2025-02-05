963f6c148a0fbf0bd8547eb0abb2e81e6117264b
Omar Karake <omar.karake@amalitechtraining.org>
2025-02-05 11:14:36 +0200

    full crud service layer using http client as it offer unit testing simplicity and interceptor

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -11,11 +11,37 @@ import { GetCoursesResponse } from '../models/get-courses.response';
 export class CoursesService {
   constructor(private readonly http: HttpClient) {}
   env = environment;
+
   loadAllCourses = async (): Promise<Course[] | undefined> => {
     const courses$ = this.http.get<GetCoursesResponse>(
       `${this.env.apiRoot}/courses`
     );
     const response = await firstValueFrom(courses$); // this will return promise of the first emited value
     return response.courses;
+  };
+
+  createCourse = async (course: Partial<Course>): Promise<Course> => {
+    const course$ = this.http.post<Course>(
+      `${this.env.apiRoot}/courses`,
+      course
+    );
+    return firstValueFrom(course$);
+    // it's best practice to remove await since we don't have try/catch block here.
+  };
+
+  saveCourse = async (
+    courseId: string,
+    course: Partial<Course>
+  ): Promise<Course> => {
+    const course$ = this.http.put<Course>(
+      `${this.env.apiRoot}/courses/${courseId}`,
+      course
+    );
+    return firstValueFrom(course$);
+  };
+
+  deleteCourse = async (courseId: string): Promise<void> => {
+    const delete$ = this.http.delete<void>(`${this.env.apiRoot}/${courseId}`);
+    return firstValueFrom(delete$);
   };
 }
