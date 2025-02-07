d05a1959939d27b555c4315be26871506ea5b443
Omar Karake <omar.karake@amalitechtraining.org>
2025-02-07 20:12:43 +0200

    implementing view single course, creating resolver and async function that serve different purpose for getting and searching lessons like query or get and using lesson resolvers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,35 +1,47 @@
-import {Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {LoginComponent} from "./login/login.component";
-import {LessonsComponent} from "./lessons/lessons.component";
-import {ResourceDemoComponent} from "./resource-demo/resource-demo.component";
-import {LinkedSignalDemoComponent} from "./linked-signal/linked-signal-demo.component";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { ResourceDemoComponent } from './resource-demo/resource-demo.component';
+import { LinkedSignalDemoComponent } from './linked-signal/linked-signal-demo.component';
 import { isUserAuthenticated } from './guards/auth.guard';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './course/course.resolver';
+import { courseLessonResolver } from './course/course-lessons.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    canActivate: [isUserAuthenticated]
+    canActivate: [isUserAuthenticated],
   },
   {
-    path: "login",
-    component: LoginComponent
+    path: 'courses/:courseId',
+    component: CourseComponent,
+    canActivate: [isUserAuthenticated],
+    resolve: {
+      course: courseResolver,
+      lessons: courseLessonResolver,
+    },
   },
   {
-    path: "lessons",
-    component: LessonsComponent
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path:"shopping-cart",
-    component: LinkedSignalDemoComponent
+    path: 'lessons',
+    component: LessonsComponent,
   },
   {
-    path: "resource-demo",
-    component: ResourceDemoComponent
+    path: 'shopping-cart',
+    component: LinkedSignalDemoComponent,
+  },
+  {
+    path: 'resource-demo',
+    component: ResourceDemoComponent,
   },
   {
     path: '**',
-    redirectTo: '/'
-  }
+    redirectTo: '/',
+  },
 ];
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -21,6 +21,13 @@ export class CoursesService {
     return response.courses;
   };
 
+  getCourseById = async (courseId: string): Promise<Course> => {
+    const course$ = this.http.get<Course>(
+      `${this.env.apiRoot}/courses/${courseId}`
+    );
+    return firstValueFrom(course$);
+  };
+
   createCourse = async (course: Partial<Course>): Promise<Course> => {
     const course$ = this.http.post<Course>(
       `${this.env.apiRoot}/courses`,
